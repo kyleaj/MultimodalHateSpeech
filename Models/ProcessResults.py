a19824f9a828bd1121c1a321eb2c25d95a19109c
Kyle Johnson <kyleaj@princeton.edu>
2020-10-20 14:27:13 -0400

    Add initial results processing script

---
+++ Models/ProcessResults.py
@@ -0,0 +1,20 @@
+import sys
+
+results_output = sys.argv[1]
+
+f = open(results_output, "r")
+
+max_acc = 0
+epoch = -1
+
+for i, line in enumerate(f):
+    if "Eval acc: " in line:
+        result = line.split("Eval acc: ")[1]
+        result = float(result)
+        if result > max_acc:
+            max_acc = result
+            epoch = i+1
+
+print("Max evaluation accuracy:")
+print(max_acc)
+print("Epoch " + str(epoch))
