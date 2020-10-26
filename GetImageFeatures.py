530e022b84600fbbe3b2619536edf34adba25d70
Kyle Johnson <kyleaj@princeton.edu>
2020-10-25 22:23:03 -0400

    Move tensor to device

--- GetImageFeatures.py
+++ GetImageFeatures.py
@@ -52,7 +52,7 @@ for i, im in enumerate(ims):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         input_tensor = preprocess(input_image)
-        input_batch = input_tensor.unsqueeze(0)
+        input_batch = input_tensor.unsqueeze(0).to(device)
         with torch.no_grad():
             output = model(input_batch)
 
