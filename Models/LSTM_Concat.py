9a317369f32e8798783f40141aa794c36f4cdd41
Kyle Johnson <kyleaj@princeton.edu>
2020-11-19 02:33:15 -0500

    Added dropout

--- Models/LSTM_Concat.py
+++ Models/LSTM_Concat.py
@@ -2,7 +2,8 @@ import torch
 
 class LSTM_Concat(torch.nn.Module):
 
-    def __init__(self, LSTM_dim, LSTM_cell_num, LSTM_bidirectional, text_embed_dim, image_embed_dim, decoder_dim, num_classes=2):
+    def __init__(self, LSTM_dim, LSTM_cell_num, LSTM_bidirectional, 
+                    text_embed_dim, image_embed_dim, decoder_dim, num_classes=2, lstm_dropout=0, decoder_dropout=0):
         super().__init__()
 
         self.LSTM = torch.nn.LSTM(
@@ -10,13 +11,14 @@ class LSTM_Concat(torch.nn.Module):
             hidden_size=LSTM_dim,
             num_layers=LSTM_cell_num,
             bidirectional=LSTM_bidirectional,
-            batch_first=True)
+            batch_first=True,
+            dropout=lstm_dropout)
 
         lstm_out = LSTM_dim
         if (LSTM_bidirectional):
             lstm_out *= 2
 
-        self.decoder = torch.nn.Linear(in_features=lstm_out, out_features=decoder_dim)
+        self.decoder = torch.nn.Linear(in_features=lstm_out, out_features=decoder_dim, dropout=decoder_dropout)
         self.classifier = torch.nn.Linear(in_features=decoder_dim, out_features=num_classes)
 
     def forward(self, text, image, lengths):
