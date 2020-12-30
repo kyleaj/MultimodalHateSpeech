3fbd9e6575cb2a00b395bd6a7ba380b768122eb4
Kyle Johnson <kyleaj@princeton.edu>
2020-12-30 12:17:56 -0500

    Updated result processor to include AUROC, fixed extra files being made

--- Models/ProcessResults.py
+++ Models/ProcessResults.py
@@ -5,9 +5,12 @@ results_output = sys.argv[1]
 f = open(results_output, "r")
 
 max_acc = 0
+max_auroc = 0
 epoch = -1
+auroc_epoch = -1
 
 process_line = False
+process_auroc = False
 curr_epoch = 0
 for line in f:
     if "Eval acc: " in line:
@@ -19,7 +22,19 @@ for line in f:
         if result > max_acc:
             max_acc = result
             epoch = curr_epoch
+    if "Eval auroc:" in line:
+        process_auroc = True
+    elif process_auroc:
+        process_auroc = False
+        result = float(line)
+        if result > max_auroc:
+            max_auroc = result
+            auroc_epoch = curr_epoch
 
 print("Max evaluation accuracy:")
 print(max_acc)
 print("Epoch " + str(epoch))
+print()
+print("Max auroc:")
+print(max_auroc*100)
+print("Epoch " + str(auroc_epoch))
