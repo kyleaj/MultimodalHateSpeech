e8a22b31982674fe51eb838406e7492ef0da4448
Kyle Johnson <kyleaj@princeton.edu>
2021-04-08 12:27:33 -0400

    Fix how dropout is applied

--- Models/MCB_PreTrainedTransformer.py
+++ Models/MCB_PreTrainedTransformer.py
@@ -14,19 +14,23 @@ class MCB_Late_Fusion(torch.nn.Module):
         self.decoder = torch.nn.Linear(in_features=512, out_features=256)
         self.classifier = torch.nn.Linear(in_features=256, out_features=num_classes)
 
+        self.drop1 = torch.nn.Dropout(0.5)
+        self.drop2 = torch.nn.Dropout(0.5)
+        self.drop3 = torch.nn.Dropout(0.2)
+
     def forward(self, text, image, lengths):
         text = self.text_process(text)
-        text = torch.nn.Dropout(0.5)(text)
+        text = self.drop1(text)
         text = torch.nn.ReLU()(text)
 
         image = self.im_process(image)
-        image = torch.nn.Dropout(0.5)(image)
+        image = self.drop2(image)
         image = torch.nn.ReLU()(image)
 
         mcb_out = self.mcb(text, image)
 
         decoder_out = self.decoder(mcb_out)
-        decoder_out = torch.nn.Dropout(0.5)(decoder_out)
+        decoder_out = self.drop3(decoder_out)
         decoder_out = torch.nn.ReLU()(decoder_out)
 
         out = self.classifier(decoder_out)
